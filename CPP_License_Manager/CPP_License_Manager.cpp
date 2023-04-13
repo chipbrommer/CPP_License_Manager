23568eb5c8dc2333131c1085c7ab4b3cbc82b8fa
Chip Brommer <chipbrommer@gmail.com>
2023-04-12 21:24:05 -0500

    Add project files.

---
+++ CPP_License_Manager/CPP_License_Manager.cpp
@@ -0,0 +1,12 @@
+﻿// CPP_License_Manager.cpp : Defines the entry point for the application.
+//
+
+#include "CPP_License_Manager.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
